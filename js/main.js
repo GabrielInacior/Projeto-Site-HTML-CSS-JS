5d8eaac9e9797e79cefa9e3c846165c0c68c2f9b
GabrielInacior <123507713+GabrielInacior@users.noreply.github.com>
2023-02-07 10:47:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    slidesToShow: 1,
+    adaptiveHeigth: true,
+    autoplay: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplaySpeed: 2000,
+    slidesToScroll: 1
+});
